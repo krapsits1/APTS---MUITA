5cfc909f21c43324a70f8177ca47d8b3bc9652c2
Emils Vetra <emilsvetra@gmail.com>
2025-03-24 09:23:56 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+struct Iebraucejs
+{
+    char tips;
+    int id;
+    int ierasanasLaiks;
+    int izejasLaiks; // Iziešanas laiks ir vērtība, ko iegūst ierašanas laikam pieskaitot laiku, kas pavadīts gaidot rindā, un laiku, kas patērēts pie muitnieka kontrolē
+};
+
+struct Muitnieks{
+    char tips;
+    int darbaAtrums;
+    int d;
+    int kadBrivs;
+};
+
+
+int main() {
+
+}
